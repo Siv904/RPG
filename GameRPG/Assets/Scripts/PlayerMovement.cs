526a9512be2187cbd98418905f80700a6544e1fd
Siv <sivgames904@gmail.com>
2022-12-02 19:00:58 +0100

    Added Character
    -Added Character
      - Animations
      - Movement
    - Fixed Camera
      - Added Camera Controller
    - Added World
    - Fixed a few bugs

--- GameRPG/Assets/Scripts/PlayerMovement.cs
+++ GameRPG/Assets/Scripts/PlayerMovement.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour
 {
 
     public float moveSpeed = 5f;
+    public Vector2 moveDirection;
+
+    public Animator animator;
+
     public Rigidbody2D rb;
 
     Vector2 movement;
@@ -15,11 +20,30 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        Animate();
     }
+
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        ProcessInputs();
+    }
+    
+    void ProcessInputs()
+    {
+        moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveDirection.Normalize();
+    }
+
+    void Animate()
+    {
+        if (moveDirection != Vector2.zero)
+        {
+            animator.SetFloat("Horizontal", moveDirection.x);
+            animator.SetFloat("Vertical", moveDirection.y);
+        }
+        animator.SetFloat("Speed", moveSpeed);
     }
 
 }
